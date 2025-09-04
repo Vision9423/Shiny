6f30f58a69f13546b86e534d88f0742a6f36421e
Vision9423 <nordeb_q@yahoo.com>
2025-09-04 10:59:58 +0900

    Delete

--- R/fun/isTruthyReactiveValues.R
+++
@@ -1,8 +0,0 @@
-isTruthyReactiveValues <- function(values) {
-  values_list <- reactiveValuesToList(values)
-  
-  isTruthyListElements <- sapply(values_list, isTruthy)
-  
-  all(isTruthyListElements)
-  
-}
