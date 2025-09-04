715324bd26b27e3b10dd57680d1ccf48c39d144a
Vision9423 <nordeb_q@yahoo.com>
2025-09-04 13:28:55 +0900

    test

--- app.R
+++ app.R
@@ -16,18 +16,27 @@ library(gtsummary)
 library(labelled)
 
 
-shiny_files <- list.files(
-  path = 'R',
-  pattern = '\\.R$',
-  recursive = TRUE,
-  full.names = TRUE
+# shiny_files <- list.files(
+#   path = 'R',
+#   pattern = '\\.R$',
+#   recursive = TRUE,
+#   full.names = TRUE
+# )
+# 
+# lapply(shiny_files, source)
+# 
+# source('ui.R')
+# source('server.R')
+
+ui <- page_navbar(
+  title = 'Test',
+  nav_panel(
+    title = 'Testing',
+    'This is nav_panel'
+  )
 )
 
-lapply(shiny_files, source)
-
-source('ui.R')
-source('server.R')
-
+server <- function(input, output, session) {}
 
 
 shinyApp(ui = ui, server = server)
